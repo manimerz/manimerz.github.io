6b74456357ecdefa7a1e07ebb539a915fba39aff
manimerz <manimerzi@gmail.com>
2020-08-08 21:28:08 +0200

    Initial commit

---
+++ component---src-pages-about-jsx-0ee1ad51c37ba444ba37.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"UA/k":function(n,t,e){},bT9l:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return i}));var u=e("q1tI"),c=e.n(u);e("UA/k");function i(){return c.a.createElement("div",null)}}}]);
+//# sourceMappingURL=component---src-pages-about-jsx-0ee1ad51c37ba444ba37.js.map
